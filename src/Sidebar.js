f0d428fca57d5767e3cafc785eb938e477473fbc
KingAlex1997 <alex97l@hotmail.com>
2022-04-25 22:55:02 -0300

    ready sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,12 +3,22 @@ import React from "react";
 import "./Sidebar.css";
 
 export const Sidebar = () => {
+  const recentItem = (topic) => (
+    <div className="sidebar_recentItem">
+      <span className="sidebar_hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar_top">
-        <img src="" alt="" />
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4D16AQEPyMqKooyQFQ/profile-displaybackgroundimage-shrink_350_1400/0/1650439743263?e=2147483647&v=beta&t=ch-XgzvvpN0LF8uzAihWPZBZBbMNjWvEXOnLciBSqy0"
+          alt=""
+        />
         <Avatar className="sidebar_avatar" />
-        <h2>Alexander</h2>
+        <h2>Alexander Fernández</h2>
         <h4>alex97l@hotmail.com</h4>
       </div>
       <div className="sidebar_stats">
@@ -20,6 +30,14 @@ export const Sidebar = () => {
           <p>Views on post</p>
           <p className="sidebar_statNumber">2,448</p>
         </div>
+      </div>
+      <div className="sidebar_bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("vue")}
+        {recentItem("ruby")}
+        {recentItem("rubyonrails")}
       </div>
     </div>
   );
