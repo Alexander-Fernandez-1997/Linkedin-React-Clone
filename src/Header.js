b385b5e47c20e51af4b26701fcf82a4f5d8655f8
KingAlex1997 <alex97l@hotmail.com>
2022-04-27 00:20:41 -0300

    responsive en celu el login

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ import HomeIcon from "@material-ui/icons/Home";
 import {
   BusinessCenter,
   Chat,
+  Menu,
   Notifications,
   SupervisorAccount,
 } from "@material-ui/icons";
@@ -14,13 +15,22 @@ import { auth } from "./firebase";
 import { logout } from "./features/userSlice";
 
 export const Header = () => {
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    let x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  };
   const dispatch = useDispatch();
   const logoutOfApp = () => {
     dispatch(logout());
     auth.signOut();
   };
   return (
-    <div className="header">
+    <div className="header topnav">
       <div className="header_left">
         <img src="https://i.imgur.com/2hiehp3.png"></img>
 
@@ -29,7 +39,10 @@ export const Header = () => {
           <input type="text" placeholder="Search" />
         </div>
       </div>
-      <div className="header_right">
+      <a href="" onClick={toggleMenu} className="icon">
+        <Menu />
+      </a>
+      <div className="header_right" id="myLinks">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccount} title="My Network" />
         <HeaderOption Icon={BusinessCenter} title="Jobs" />
